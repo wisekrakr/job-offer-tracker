2a448dd61f486b61a4202860b3940cc2312d22d5
David <wisekrakr@gmail.com>
2021-05-28 01:55:53 +0200

    added json-server

--- src/App.js
+++ src/App.js
@@ -1,77 +1,148 @@
-import {useState} from 'react'
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Offers from "./components/Offers";
 import AddOffer from "./components/AddOffer";
-
+import About from "./components/About";
 
 function App() {
-  const [showAddOffer, setShowAddOffer] = useState(false)
-  const [offers, setOffers] = useState([
-    {
-        id: 1,
-        job: "Java developer",
-        business: "Cool Blue",
-        description: "Low level, HBO, Java, back-end",
-        hours: '32 hours',
-        salary: '€3.000',
-        original_date: "May 16th",
-        added_date: "May 20th at 16:00",
-        apply_date: "May 22nd at 13:00",
-        link:"",
-        reminder: false,
-        concluded: false
-    },
-    {
-        id: 2,
-        job: "React.js developer",
-        business: "Cool Blue",
-        description: "MIddle level, HBO, React, full stack",
-        hours: '40 hours',
-        salary: '€3.500',
-        original_date: "May 17th",
-        added_date: "May 22th at 13:45",
-        apply_date: "May 26nd at 17:00",
-        link:"",
-        reminder: true,
-        concluded: false
-    }
+  const [showAddOffer, setShowAddOffer] = useState(false);
+  const [offers, setOffers] = useState([]);
 
-  ])
+  useEffect(() => {
+    const getOffers = async () => {
+      const offersFromServer = await fetchOffers();
+      setOffers(offersFromServer);
+    };
 
+    getOffers();
+  }, []);
 
-  const addJobOffer = (offer) =>{
-    const id = Math.floor(Math.random() * 10000) + 1
+  // Get all Job Offers
+  const fetchOffers = async () => {
+    const res = await fetch("http://localhost:5000/offers");
 
-    const jobOffer = {id, ...offer}
-    setOffers([...offers, jobOffer])
-  }
+    return await res.json();
+  };
 
-  const deleteOffer = (id)=>{
-    setOffers(offers.filter((offer) => offer.id !== id))
-  }
+  // Get a single Job Offer
+  const fetchOffer = async (id) => {
+    const res = await fetch(`http://localhost:5000/offers/${id}`, {
+      method: "GET",
+    });
 
-  const concludeOffer = (id)=>{
-    setOffers(offers.map((offer) => offer.id === id ? {...offer, concluded: !offer.concluded} : offer))
-  }
+    return await res.json();
+  };
 
-  const toggleReminder = (id)=>{
-    setOffers(offers.map((offer) => offer.id === id ? {...offer, reminder: !offer.reminder} : offer))
-  }
+  // Add a Job Offer
+  const addJobOffer = async (offer) => {
+    const res = await fetch("http://localhost:5000/offers/", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(offer),
+    });
+
+    const data = await res.json();
+
+    // Get all offers and add "data" onto it (the new offer)
+    setOffers([...offers, data]);
+  };
+
+  // Delete a Job Offer
+  const deleteOffer = async (id) => {
+    await fetch(`http://localhost:5000/offers/${id}`, {
+      method: "DELETE",
+    });
+
+    setOffers(offers.filter((offer) => offer.id !== id));
+  };
+
+  // Set Job Offer to Concluded
+  const concludeOffer = async (id) => {
+    const offerToConclude = await fetchOffer(id);
+    const updatedOffer = {
+      ...offerToConclude,
+      concluded: !offerToConclude.reminder,
+    };
+
+    const res = await fetch(`http://localhost:5000/offers/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedOffer),
+    });
+
+    const data = await res.json();
+
+    setOffers(
+      offers.map((offer) =>
+        offer.id === id ? { ...offer, concluded: !data.concluded } : offer
+      )
+    );
+  };
+
+  // Toggle Job Offer Reminder on/off
+  const toggleReminder = async (id) => {
+    const offerToToggle = await fetchOffer(id);
+    const updatedOffer = {
+      ...offerToToggle,
+      reminder: !offerToToggle.reminder,
+    };
+
+    const res = await fetch(`http://localhost:5000/offers/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedOffer),
+    });
+
+    const data = await res.json();
+
+    setOffers(
+      offers.map((offer) =>
+        offer.id === id ? { ...offer, reminder: !data.reminder } : offer
+      )
+    );
+  };
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddOffer(!showAddOffer)} showAddOffer={setShowAddOffer} title="Job Offer Tracker"/>
-        {showAddOffer && <AddOffer onAdd={addJobOffer}/> }
-        {offers.length > 0 ? 
-        <Offers 
-          offers={offers} 
-          onDelete={deleteOffer}
-          onConcluded={concludeOffer} 
-          onToggle={toggleReminder}
-          /> :
-         <p>No Job Offers added</p>}
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddOffer(!showAddOffer)}
+          showAddOffer={setShowAddOffer}
+          title="Job Offer Tracker"
+        />
+
+        <Route
+          path="/"
+          exact
+          render={(props) => (
+            <>
+              {showAddOffer && <AddOffer onAdd={addJobOffer} />}
+              {offers.length > 0 ? (
+                <Offers
+                  offers={offers}
+                  onDelete={deleteOffer}
+                  onConcluded={concludeOffer}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                <p>No Job Offers added</p>
+              )}
+            </>
+          )}
+        />
+        <Route path="/about" component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
